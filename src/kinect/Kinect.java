06789b1b459eabd08cf8125472b6da26d5660eeb
Martin <mr.mmaurer@gmail.com>
2012-11-01 22:13:36 +0100

    Removed calculation of scalled hands and all body tracking

--- src/kinect/Kinect.java
+++ src/kinect/Kinect.java
@@ -1,60 +1,56 @@
 package kinect;
+
 import java.util.ArrayList;
 
 import processing.core.PApplet;
 import SimpleOpenNI.SimpleOpenNI;
 
-public class Kinect extends PApplet {	
+@SuppressWarnings("serial")
+public class Kinect extends PApplet {
 	public SimpleOpenNI kinect;
 	public PApplet p;
-	public ArrayList<KinectUser> users= new ArrayList<KinectUser>();	
-	
-	public Kinect(PApplet p) {	
-		this.p=p;
-		this.kinect=new SimpleOpenNI(p,SimpleOpenNI.RUN_MODE_MULTI_THREADED);		
-		this.settings();		
-		
+	public ArrayList<KinectUser> users = new ArrayList<KinectUser>();
+
+	public Kinect(PApplet p) {
+		this.p = p;
+		this.kinect = new SimpleOpenNI(p, SimpleOpenNI.RUN_MODE_MULTI_THREADED);
+		this.settings();
+
 	}
-	
-	public SimpleOpenNI getKinect(){
+
+	public SimpleOpenNI getKinect() {
 		if (kinect.init()) {
 			return kinect;
-		}
-		else return null;
+		} else
+			return null;
 	}
-	
-	
-	public ArrayList<KinectUser> getUserListe(){		
+
+	public ArrayList<KinectUser> getUserListe() {
 		return users;
 	}
-	
-	private void settings(){ 
-	  // Quellen aktivieren
-      kinect.enableDepth();  
-	  //context.enableIR();
-      kinect.enableRGB();
-      kinect.enableHands();
-      kinect.enableGesture();
-	  
-	  // Alle Bones
-      kinect.enableUser(SimpleOpenNI.SKEL_PROFILE_ALL);
-	  //context.enableUser(SimpleOpenNI.SKEL_PROFILE_HEAD_HANDS);
-	  
-	  // Spiegeln
-      kinect.setMirror(true);
-	  
-	  // Gesten
-      kinect.addGesture("Wave");
-      kinect.addGesture("Click");
-	  kinect.addGesture("RaiseHand");  
-	  
-	  //Enable Scene
-	  kinect.enableScene(640,480,60);	  
-	  
-	  //return context;
-	}
-	
-	
 
+	private void settings() {
+		// Quellen aktivieren
+		kinect.enableDepth();
+		// context.enableIR();
+		kinect.enableRGB();
+		kinect.enableHands();
+		kinect.enableGesture();
+
+		// Alle Bones
+		// kinect.enableUser(SimpleOpenNI.SKEL_PROFILE_ALL);
+		kinect.enableUser(SimpleOpenNI.SKEL_PROFILE_HEAD_HANDS);
+
+		// Spiegeln
+		kinect.setMirror(true);
+
+		// Gesten
+		// kinect.addGesture("Wave");
+		// kinect.addGesture("Click");
+		// kinect.addGesture("RaiseHand");
+
+		// Enable Scene
+		kinect.enableScene(640, 480, 60);
+	}
 
 }
--- src/kinect/KinectUser.java
+++ src/kinect/KinectUser.java
@@ -1,63 +1,65 @@
 package kinect;
+
 import processing.core.PApplet;
 import processing.core.PVector;
 import SimpleOpenNI.SimpleOpenNI;
 
 public class KinectUser {
 	public SimpleOpenNI kinect;
-	public int userId;	
+	public int userId;
 	public PApplet p;
 	public PVector leftHand;
 	public PVector rightHand;
-	
-	public KinectUser(PApplet p, SimpleOpenNI kinect, int userId){
-		this.kinect=kinect;
-		this.userId=userId;
-		this.p= p;	
+
+	public KinectUser(PApplet p, SimpleOpenNI kinect, int userId) {
+		this.kinect = kinect;
+		this.userId = userId;
+		this.p = p;
 	}
-	
-	public PVector getLeftHand(){		
+
+	public PVector getLeftHand() {
 		return leftHand;
 	}
-	
-	public PVector getRightHand(){		
+
+	public PVector getRightHand() {
 		return rightHand;
 	}
-	
-	public void updateLimbs(){		
+
+	public void updateLimbs() {
 		PVector rightHandReal = new PVector();
-		PVector rightHandProjected =  new PVector();
-		
+		PVector rightHandProjected = new PVector();
+
 		PVector leftHandReal = new PVector();
-		PVector leftHandProjected =  new PVector();
-		
-		PVector leftHandProjectedUnscalled = new PVector();
-		PVector rightHandProjectedUnscalled =  new PVector();
-		
-		//Handposition auslesen 
-		kinect.getJointPositionSkeleton(userId, SimpleOpenNI.SKEL_RIGHT_HAND,rightHandReal);
-		kinect.getJointPositionSkeleton(userId, SimpleOpenNI.SKEL_LEFT_HAND,leftHandReal);
+		PVector leftHandProjected = new PVector();
+
+		// PVector leftHandProjectedUnscalled = new PVector();
+		// PVector rightHandProjectedUnscalled = new PVector();
+
+		// Handposition auslesen
+		kinect.getJointPositionSkeleton(userId, SimpleOpenNI.SKEL_RIGHT_HAND,
+				rightHandReal);
+		kinect.getJointPositionSkeleton(userId, SimpleOpenNI.SKEL_LEFT_HAND,
+				leftHandReal);
 
 		// Pos Konvertieren
 		kinect.convertRealWorldToProjective(rightHandReal, rightHandProjected);
-		kinect.convertRealWorldToProjective(rightHandReal, rightHandProjectedUnscalled);
-		
 		kinect.convertRealWorldToProjective(leftHandReal, leftHandProjected);
-		kinect.convertRealWorldToProjective(leftHandReal, leftHandProjected);
-		kinect.convertRealWorldToProjective(leftHandReal, leftHandProjectedUnscalled);		
-		
-		p.color(123,123,123);
-		p.fill(0,255);	
 
-		// SKalierungsfaktor fuer 3D
-		//float sfaktorr= (p.map(rightHandProjected.z,300,3000,7,1));			
+		// kinect.convertRealWorldToProjective(rightHandReal,
+		// rightHandProjectedUnscalled);
+		// kinect.convertRealWorldToProjective(leftHandReal,
+		// leftHandProjectedUnscalled);
 
-		// Handboxen zeichnen x y z
-		float rectSize=50;		
-		p.rect(leftHandProjected.x-rectSize/2,leftHandProjected.y-rectSize/2,rectSize,rectSize);
-		p.rect(rightHandProjected.x-rectSize/2,rightHandProjected.y-rectSize/2,rectSize,rectSize);		
-		
-		this.rightHand=rightHandProjectedUnscalled;
-		this.leftHand=leftHandProjectedUnscalled;		
-	}	
+		// Handmittelpunkt
+		p.color(123, 123, 123);
+		p.fill(0, 255);
+		float rectSize = 10;		
+		p.rect(leftHandProjected.x - rectSize / 2, leftHandProjected.y
+				- rectSize / 2, rectSize, rectSize);
+		p.rect(rightHandProjected.x - rectSize / 2, rightHandProjected.y
+				- rectSize / 2, rectSize, rectSize);
+
+		this.leftHand = leftHandProjected;
+		this.rightHand = rightHandProjected;
+	}
 }
